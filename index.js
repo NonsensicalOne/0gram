0839a09f5a1d40cd30c77f3a303c60394d19db39
NonsensicalOne <hawlidoge@protonmail.com>
2025-06-25 21:26:19 +0300

    weird solution for private accounts

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 const headers = require("./.headers.js");
+const { time } = require("console");
+const { get } = require("http");
 
 app.get("/", (req, res) => {
   if (req.query.username) {
@@ -107,10 +109,11 @@ app.get("/:username", async (req, res) => {
       method: "POST",
       headers: headers,
       credentials: "include",
-      body: `av=17841475430069406&__d=www&__user=0&__a=1&__req=8&__hs=20264.HYP%3Ainstagram_web_pkg.2.1...0&dpr=3&__ccg=GOOD&__rev=1024172561&__s=phc6cx%3Amtif09%3Aw6foa6&__hsi=7519818450704458413&__dyn=7xeUjG1mxu1syUbFp41twWwIxu13wvoKewSAwHwNw9G2S7o1g8hw2nVE4W0qa0FE2awgo9oO0n24oaEnxO1ywOwv89k2C1Fwc60D87u3ifK0EUjwGzEaE2iwNwmE2eUlwhEe87q0oa2-azo7u3vwDwHg2ZwrUdUbGwmk0zU8oC1Iwqo5p0OwUQp1yUb8jxKi2K7E5y4UrwHwcObyohw4rxO2Cq2K&__csr=gD1l7_58dN2f2a6lLFeZGChQAGFvVcZRFKiXF5AFi5Ay8zp-Gp6FpkmV9e4qF4BGuAFdxh6CiKpGQvG_heWx7mBXJ7RKh2AXjx6iQ9UKeAJGbACVEZfGCU8HgS2it7xKKp2aLzVuuUiGjyWy8NK4GBhlCKAiECayVK5em49Xy8KvxW36eCxh28S00l5O1jxm0cUxe5t0du3G1pixa1SwN7d9o9m0EE2C80kW0H625em0cfw1nK0pO2Vaut0IwbJ0hU12E6-u5y0cy0_64A2B5wEPDwee1Gw4y8i8xh0r65nh82Ywq83yw3t401MYw0zNw1M-&__hsdp=l0MMQO59srdb5hklekXIb1jRGbIAmcinAy85ep8i5ohZy6Gm29QkN6rc8P5o71huUmAwsSbKi1oUOi7UEzxi2e9xe4qyp8lwQwQCgx0Ke9wvEO5E3owhU8EcEtyUnKUjxi1bCwhU0ym4E18U1Po2jwCwMw826o3cyU2IwvE2BwXwcAwtx2q782ew-wnE8E5uEqwWAsw2_xS0yU62&__hblp=0lUdU6q2d0dKaz83cwFwqGzEnBwYxa2y8yVodUG4XGal29axa4E8o9UthocFokzUO1cz8mwHU2ywj-222S4ueG8xuu4UGFEa88FE4u1awWwKAwd60CoiCJ0h87O1lzE1lo6y2i1pxi7oy2yu0w8pxSU5uewgUK0H8swoo2BwXwcAwtx2q7e487K3W1uguG322qE_Aw-Asw884u7ob8yEtw8F0AyEc8&__comet_req=7&fb_dtsg=NAftK0LssKo4TD7ECjYgCLJ--92h5H8FwxDbNS5lglOLtwX9l4JO8qg%3A17865145036029998%3A1750843419&jazoest=26033&lsd=KAx5LvmccjkRbvnrGwtruA&__spin_r=1024172561&__spin_b=trunk&__spin_t=1750844170&__crn=comet.igweb.PolarisProfilePostsTabRoute&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisProfilePostsQuery&variables=%7B%22data%22%3A%7B%22count%22%3A12%2C%22include_reel_media_seen_timestamp%22%3Atrue%2C%22include_relationship_info%22%3Atrue%2C%22latest_besties_reel_media%22%3Atrue%2C%22latest_reel_media%22%3Atrue%7D%2C%22username%22%3A%22${username}%22%2C%22__relay_internal__pv__PolarisIsLoggedInrelayprovider%22%3Atrue%2C%22__relay_internal__pv__PolarisShareSheetV3relayprovider%22%3Atrue%7D&server_timestamps=true&doc_id=23905326119127169`,
+      body: `av=17841475430069406&__d=www&__user=0&__a=1&__req=8&__hs=20264.HYP%3Ainstagram_web_pkg.2.1...0&dpr=3&__ccg=GOOD&__rev=1024176170&__s=d9jd7v%3An7v1wu%3Aty7h6h&__hsi=7519944860177051201&__dyn=7xeUjG1mxu1syUbFp41twWwIxu13wvoKewSAwHwNw9G2S7o1g8hw2nVE4W0qa0FE2awgo9oO0n24oaEnxO1ywOwv89k2C1Fwc60D87u3ifK0EUjwGzEaE2iwNwmE2eUlwhEe87q0oa2-azo7u3vwDwHg2ZwrUdUbGwmk0zU8oC1Iwqo5p0OwUQp1yUb8jxKi2K7E5y4UrwHwcObyohw4rxO2Cq2K&__csr=gNd1tsqMH7hsR9bsA-mWjjNOVXl4R9lX8pi4VeVaBLjCKcVKHAjHQmBG8hA59fALy4imqmj-UGBKJ6Z2KF5GCiuWDBF94GdmZ3Uxe8Gay8y88V9bzKXnzVXKFoaoqzkXz5yVpEgzlKEKXJ2bizGWy8DxzCAy4cleK4pe8x65A2uqFVE98jx6bF1e00k_90q80VVG4E3OwYwlWx90tEc1cSlo8OwaG0CUf82zg7a0F9A5EOV80Li05zo1D8dACdwLwbJ0hU12E7t1kw39wfJx90FCzEnIg0le0i8M8o6up3k8l0b-1Eo3ww0vIo08Wo0smw&__hsdp=l0OgKO5s8iiEr5hkVjGgb1ahuFFZT44hkppE5uh2Yg48gBrqmESeim9cQHe4sQq1vgKFoWh0rologxK3W2F3Vi1GiEoxGaxijG0CQ4106l1uvwg9oOi0gK9G4UKeK4VrV9U6SE2Uw4Ow860hS0nK0XU2uwqEW1TG1axa0Ao7S0M8gwdQweE6K3q6827xy1nxKexoUW0Xoc823x2&__hblp=0lU5u19wwg3vK5U2-wFwqESawMwLBDwVVo8ErDyVHCzpFeFe4o2kwx-16xt7UkG2t5z98coiwuUCEG3a9G4UK4EtWUeUaEWEqwjobo8o1cEc8cUsx22e1Gwvo3JzU792o7e1XwkofoW2a17DCx61dwXwCG4E2hxO1wwc2483t83G6U4-2Gbxy0w9EoxG3vGE9UmeiV4ex-1awsWwJwrUjx24U&__comet_req=7&fb_dtsg=NAfvXPfQU96ZPduWRTIVelu8PeqjKtOkI8RQ_rWQtNPpxY6YxtCGJxQ%3A17864642926059691%3A1750868749&jazoest=26476&lsd=gC4tcamrET8JQk6f_ljn_1&__spin_r=1024176170&__spin_b=trunk&__spin_t=1750873602&__crn=comet.igweb.PolarisProfilePostsTabRoute&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisProfilePostsQuery&variables=%7B%22data%22%3A%7B%22count%22%3A12%2C%22include_reel_media_seen_timestamp%22%3Atrue%2C%22include_relationship_info%22%3Atrue%2C%22latest_besties_reel_media%22%3Atrue%2C%22latest_reel_media%22%3Atrue%7D%2C%22username%22%3A%22${username}%22%2C%22__relay_internal__pv__PolarisIsLoggedInrelayprovider%22%3Atrue%2C%22__relay_internal__pv__PolarisShareSheetV3relayprovider%22%3Atrue%7D&server_timestamps=true&doc_id=23905326119127169`,
     });
 
     const responseText = await response.text();
+
     const contentType = response.headers.get("content-type") || "";
     if (!contentType.includes("application/json")) {
       throw new Error(
@@ -118,13 +121,29 @@ app.get("/:username", async (req, res) => {
       );
     }
     const info = JSON.parse(responseText);
-
-    const timelineData =
-      info.data.xdt_api__v1__feed__user_timeline_graphql_connection;
+    const timelineData = info.data?.xdt_api__v1__feed__user_timeline_graphql_connection || "";
     if (!timelineData || !timelineData.edges) {
       return res.render("profile.ejs", {
         user: { edge_owner_to_timeline_media: { count: 0, edges: [] } },
         info: {},
+      });
+    }
+
+    let userId = info.data?.xdt_api__v1__feed__user_timeline_graphql_connection?.edges?.[0]?.node?.user?.pk ?? "nouserid";
+    // second method to retrieve someone's ID
+    if (userId == "nouserid") {
+      const searchResponse = await fetch(`https://www.instagram.com/web/search/topsearch/?query=${username}`, {
+        "headers": headers,
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET"
+      });
+
+      const responseJson = await searchResponse.json();
+      responseJson.users.forEach(mf => {
+        if (mf.user.username == username) {
+          userId = mf.user.id
+        }
       });
     }
 
@@ -158,7 +177,7 @@ app.get("/:username", async (req, res) => {
       {
         method: "POST",
         headers: headers,
-        body: `av=17841475430069406&__d=www&__user=0&__a=1&__req=4&__hs=20264.HYP%3Ainstagram_web_pkg.2.1...0&dpr=3&__ccg=GOOD&__rev=1024174358&__s=wc92gw%3Aijjq08%3Aff1dmr&__hsi=7519844879460866429&__dyn=7xe5WwlEnwn8K2Wmm1twpUnwgU7S6EdF8aUco38w5ux609vCwjE1EE2Cw8G11wBw5Zx62G3i1ywOwv89k2C0iK0D82YK0EUjwGzEaE2iwNwmE2exu16wUw7VwLyES1TwTwFwIwbS1LwTwKG1pg2Xwr86C1mg6LhA6bwg8rAwHxW1oxe6UaU3cyUrw4rxO2Cq2K&__csr=n3A6dNAAj6iPhYWn9iQCy-imGvGAJA-TTeHGVvBGW9CWF3p-ZBZ9pF9O4xd2pap4by-HKq-iGgDLAioyqWlbVFJ6AVkLy9uip7AG4GAWU-BVUKmFot_jGqVESUvhE-JbAxKdUKQuEy54nCxqfGvVKiqFVel7yoLQ9UgAye8Gu694eG223mcyEjDg01jXk1Ew3DCEiwfa3O1nG4A1SwM4Pplwza0GE2rwYwad0sE2ACgmzbAw2Z80mdw6swSihFUbE2Xg4u0gG1SUl80Oo3Xoigakm2uzpE1jo18yz8kg6ul3ExkU2Ywq40Uo07X602eC075E&__hsdp=l0NMLExiNdsh24A8jBbiIb1iBhFFi8mh5hBy8fEuhV10WfCqmGqx-Ah4dJ6esF4C1JIkhUiBwi8d7xm3i68qgoxEzxW48_wAmbK1mwg44odPzofUO3C3h1S5oih8aE6u1nxy1vx2E1Fo16E3cw24Eb84K0ku1DxS1-G3W6U2sw9m0TojwaiS2-6-1RwtU2ywBwrErzPpA0U8nwaau17w&__hblp=0lU4G1txB0d-m583kxq1ByEgwjGx6m2Wm7rwJy8zxDBFoK-48fF8W1rwQxW7KcGcwFwQgC4UlGfh8aE6G6VEcpEhwsay82qwj86y1bw4qxK5Eeo5-q584-0l-i11wIxFbwGwfS19y8twg8tV87e2m7Urw9O3S1nwdS4U2AJwLxLwmorwiobo7632bxC6UpwEzUrzN1Acz84G12wHz8nw9Oh7zUnx-&__comet_req=7&fb_dtsg=NAfvDYea16_0SbijWFyFDEyLXUfyUYrKtA7a8QP8ld7XM6YebCyLBnQ%3A17865145036029998%3A1750843419&jazoest=26231&lsd=27eySuQCFG_OFp3xawnPn5&__spin_r=1024174358&__spin_b=trunk&__spin_t=1750850323&__crn=comet.igweb.PolarisProfilePostsTabRoute&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisProfilePageContentQuery&variables=%7B%22id%22%3A%22${info.data.xdt_api__v1__feed__user_timeline_graphql_connection.edges[0].node.user.pk}%22%2C%22render_surface%22%3A%22PROFILE%22%7D&server_timestamps=true&doc_id=9916454141777118`,
+        body: `av=17841475430069406&__d=www&__user=0&__a=1&__req=4&__hs=20264.HYP%3Ainstagram_web_pkg.2.1...0&dpr=3&__ccg=GOOD&__rev=1024174358&__s=wc92gw%3Aijjq08%3Aff1dmr&__hsi=7519844879460866429&__dyn=7xe5WwlEnwn8K2Wmm1twpUnwgU7S6EdF8aUco38w5ux609vCwjE1EE2Cw8G11wBw5Zx62G3i1ywOwv89k2C0iK0D82YK0EUjwGzEaE2iwNwmE2exu16wUw7VwLyES1TwTwFwIwbS1LwTwKG1pg2Xwr86C1mg6LhA6bwg8rAwHxW1oxe6UaU3cyUrw4rxO2Cq2K&__csr=n3A6dNAAj6iPhYWn9iQCy-imGvGAJA-TTeHGVvBGW9CWF3p-ZBZ9pF9O4xd2pap4by-HKq-iGgDLAioyqWlbVFJ6AVkLy9uip7AG4GAWU-BVUKmFot_jGqVESUvhE-JbAxKdUKQuEy54nCxqfGvVKiqFVel7yoLQ9UgAye8Gu694eG223mcyEjDg01jXk1Ew3DCEiwfa3O1nG4A1SwM4Pplwza0GE2rwYwad0sE2ACgmzbAw2Z80mdw6swSihFUbE2Xg4u0gG1SUl80Oo3Xoigakm2uzpE1jo18yz8kg6ul3ExkU2Ywq40Uo07X602eC075E&__hsdp=l0NMLExiNdsh24A8jBbiIb1iBhFFi8mh5hBy8fEuhV10WfCqmGqx-Ah4dJ6esF4C1JIkhUiBwi8d7xm3i68qgoxEzxW48_wAmbK1mwg44odPzofUO3C3h1S5oih8aE6u1nxy1vx2E1Fo16E3cw24Eb84K0ku1DxS1-G3W6U2sw9m0TojwaiS2-6-1RwtU2ywBwrErzPpA0U8nwaau17w&__hblp=0lU4G1txB0d-m583kxq1ByEgwjGx6m2Wm7rwJy8zxDBFoK-48fF8W1rwQxW7KcGcwFwQgC4UlGfh8aE6G6VEcpEhwsay82qwj86y1bw4qxK5Eeo5-q584-0l-i11wIxFbwGwfS19y8twg8tV87e2m7Urw9O3S1nwdS4U2AJwLxLwmorwiobo7632bxC6UpwEzUrzN1Acz84G12wHz8nw9Oh7zUnx-&__comet_req=7&fb_dtsg=NAfvDYea16_0SbijWFyFDEyLXUfyUYrKtA7a8QP8ld7XM6YebCyLBnQ%3A17865145036029998%3A1750843419&jazoest=26231&lsd=27eySuQCFG_OFp3xawnPn5&__spin_r=1024174358&__spin_b=trunk&__spin_t=1750850323&__crn=comet.igweb.PolarisProfilePostsTabRoute&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisProfilePageContentQuery&variables=%7B%22id%22%3A%22${userId}%22%2C%22render_surface%22%3A%22PROFILE%22%7D&server_timestamps=true&doc_id=9916454141777118`,
       },
     );
 

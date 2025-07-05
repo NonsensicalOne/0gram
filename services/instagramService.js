6b288576690ab8c89d996cf9b57321b7bdbabaa0
NonsensicalOne <126889064+NonsensicalOne@users.noreply.github.com>
2025-07-05 13:16:42 +0300

    add lsd integration to look less suspicious

--- services/instagramService.js
+++ services/instagramService.js
@@ -1,5 +1,10 @@
+const crypto = require("crypto");
 const fetch = require("undici").fetch;
-const headers = require("../config/headers");
+let headers = require("../config/headers");
+
+function setLsdHeader(lsd) {
+  headers = { ...headers, "x-fb-lsd": lsd };
+}
 
 async function postJSON(url, body) {
   const res = await fetch(url, {
@@ -52,6 +57,8 @@ async function fetchImage(url) {
 
 // Fetch a user's posts with pagination
 async function fetchPosts(username, after = "") {
+  setLsdHeader(headers["x-fb-lsd"]);
+
   const paginationRes = await fetch("https://www.instagram.com/graphql/query", {
     headers: headers,
     body: `av=17841475430069406&__d=www&__user=0&__a=1&__req=z&__hs=20266.HYP%3Ainstagram_web_pkg.2.1...0&dpr=3&__ccg=GOOD&__rev=1024253163&__s=fl2av0%3Areu7c1%3An9h5ma&__hsi=7520492133797078565&__dyn=7xeUjG1mxu1syUbFp41twWwIxu13wvoKewSAwHwNw9G2S7o1g8hw2nVE4W0qa0FE2awgo9oO0n24oaEnxO1ywOwv89k2C1Fwc60D87u3ifK0EUjwGzEaE2iwNwmE2eUlwhEe87q0oa2-azo7u3vwDwHg2ZwrUdUbGwmk0zU8oC1Iwqo5p0OwUQp1yUb8jxKi2qi7E5y4UrwHwGwa6byohw4rxO2Cq2K&__csr=gmMoEp6RsGYj2cRYGsAJbpa8xfEh95uDFkj-Th_h-SEy-iAi-Hpp8NbyfqZWVlAh2B-l35prpVHTUyCiF4h4gzXBC-GVKvh6ny-9AhXCC-m9wxyUGnxbQ2quQqqvy8WuvXhFbgF5G4oHQ9AyqiBwSgO8GiFerh98-9Dm8wIxZeaLyFoyUG5p8pGueCyokyZ005oeCl04VO04Bo1heexq3-OQu1kCzxcw0E6vx-US220Ao5nodU0C20i204ME0EW3i48aZwiQ1Iw5fIHy4cg7G7EG19xe5o9Ux0qUx2Wc0maE1LO4y45oaEa8922k8g31xeaA874E8y01UC4ajwlEZw0rEE0V6094w0Elw&__hsdp=gfX8kr5Olklk9PJKDEc1qaGRdOmIPbt3o5kl6l4mbrSjiEzzUZddZahEUYDh61AgMm4Gz9A2219x9Dzoeooy8sU-a86U9UR1irx10k85133wWx-2m1cwyx-dCxy2m7UbU-0w8nxbDCBw6sU2Dw6Zw5pzo7S7E6K1dwHwnK0QE9U2cw8KU3uwDw94wcokwb21vwRzoeFUKfwDxIweo7S7Edo&__hblp=0joC2q261izUak26i0z8Ony9Q4bwby2q4FE8poC2i9yp8S5UfXzUGu58Z2po8bCByEhgCuAnzSi15BxW264U4G58c9o9HBxm25a5V8Sq68vLzbzK2Sfw825UiVVFk12Bw9C0KecwFwUxy7EhwbO0TUW13w4lzpHg767E6K3-dwBBzUqwCwbC2u2u7U6O0yXxOawbqm265UfUei0NG4o5u1ownU9EV39EO2qubxaq6-6O6xGdz87zxW3m&__comet_req=7&fb_dtsg=NAfsw0EOggoIKtvgXaHs1NkJXX1AoXrShPZHnpeLWS15KtqqHKtP7PQ%3A17864642926059691%3A1750868749&jazoest=26388&lsd=RhYoGIS_R7FJmdDDrR8XVd&__spin_r=1024253163&__spin_b=trunk&__spin_t=${Math.floor(Date.now() / 1000)}&__crn=comet.igweb.PolarisProfilePostsTabRoute&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisProfilePostsTabContentQuery_connection&variables=%7B%22after%22%3A%22${after}%22%2C%22before%22%3Anull%2C%22data%22%3A%7B%22count%22%3A12%2C%22include_reel_media_seen_timestamp%22%3Atrue%2C%22include_relationship_info%22%3Atrue%2C%22latest_besties_reel_media%22%3Atrue%2C%22latest_reel_media%22%3Atrue%7D%2C%22first%22%3A12%2C%22last%22%3Anull%2C%22username%22%3A%22ichisansfw%22%2C%22__relay_internal__pv__PolarisIsLoggedInrelayprovider%22%3Atrue%2C%22__relay_internal__pv__PolarisShareSheetV3relayprovider%22%3Atrue%7D&server_timestamps=true&doc_id=24796768439926139`,
@@ -94,6 +101,8 @@ async function fetchPosts(username, after = "") {
 
 // Fetch comments
 async function fetchComments(postId) {
+  setLsdHeader(headers["x-fb-lsd"]);
+
   try {
     const res = await fetch(
       `https://www.instagram.com/api/v1/media/${postId}/comments/?can_support_threading=true&permalink_enabled=false`,
@@ -122,6 +131,9 @@ async function fetchComments(postId) {
 async function fetchProfile(username) {
   let userId;
   let jazoest;
+  let lsd;
+
+  /* <script id="__eqmc" type="application/json" nonce="">{"u":"\/ajax\/qm\/?__a=1&__user=0&__comet_req=15&jazoest=2987","e":"7523509150521564177","s":"XInstagramLoginSyncController","w":0,"f":null,"l":"AVr0SnTu_Fs"}</script> */
 
   if (!userId) {
     const page = await fetch(`https://www.instagram.com/${username}`, {
@@ -137,6 +149,17 @@ async function fetchProfile(username) {
       const valueEnd = html.indexOf('"', valueStart);
       userId = html.substring(valueStart, valueEnd);
     }
+
+    const lsdSearchString = '["LSD",[],{"token":"';
+    const lsdStartIndex = html.indexOf(lsdSearchString);
+
+    if (lsdStartIndex !== -1) {
+      const lsdValueStart = lsdStartIndex + lsdSearchString.length;
+      const lsdValueEnd = html.indexOf('"', lsdValueStart);
+      lsd = html.substring(lsdValueStart, lsdValueEnd);
+    }
+
+    setLsdHeader(lsd);
 
     // Extract jazoest from the same HTML response
     const jazoestSearchString = "jazoest=";
